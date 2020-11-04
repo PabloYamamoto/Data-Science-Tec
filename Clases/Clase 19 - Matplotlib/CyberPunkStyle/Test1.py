c31011371f5cd1a8708856b60b88d69f98df4552
Pablo Yamamoto <a01022382@gmail.com>
2020-11-03 19:53:37 -0600

    Funcional

--- Clases/Clase 19 - Matplotlib/CyberPunkStyle/Test1.py
+++ Clases/Clase 19 - Matplotlib/CyberPunkStyle/Test1.py
@@ -36,13 +36,6 @@ def Graph(data):
                 color=colors)
 
 
-    # Colorear el área de los datos
-    for column, color in zip(df, colors):
-        ax.fill_between(x=df.index,
-                        y1=df[column].values,
-                        y2=[0] * len(df),
-                        color=color,
-                        alpha=0.1)
     
     # Graficar los datos
     
