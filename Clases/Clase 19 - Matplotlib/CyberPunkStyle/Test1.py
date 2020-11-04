49c94297df390d5f178d0df798baa371ba592529
Pablo Yamamoto <a01022382@gmail.com>
2020-11-03 19:34:26 -0600

    Prueba 1 con estilo Cyberpunk

---
+++ Clases/Clase 19 - Matplotlib/CyberPunkStyle/Test1.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.style.use("seaborn-dark") # Añade un gris de fondo a la gráfica
+
+
+def Graph(data):
+
+    for param in ['figure.facecolor', 'axes.facecolor', 'savefig.facecolor']:
+        plt.rcParams[param] = '#212946'  # Color del fondo, toda la figura
+    for param in ['text.color', 'axes.labelcolor', 'xtick.color', 'ytick.color']:
+        plt.rcParams[param] = 'white'  # Color de las etiquetas y números de los ejes
+
+    colors = [
+    '#08F7FE',  # teal/cyan
+    '#FE53BB',  # pink
+    '#F5D300',  # yellow
+    '#00ff41',  # matrix green
+    ]
+
+    df = data['SAT'] # Graficamos solo una variable de data, en este caso SAT (tenemos GPA tambien)
+    
+    fig, ax = plt.subplots()
+    
+    df.plot(marker='o', color=colors, ax=ax)
+
+    # Esta parte del código nos da las sombras alrededor de las líneas graficadas, es un efecto sombra. 
+    n_shades = 5
+    diff_linewidth = 1.05
+    alpha_value = 0.3 / n_shades
+    for n in range(1, n_shades+1):
+        df.plot(marker='o',
+                linewidth=2+(diff_linewidth*n),
+                alpha=alpha_value,
+                legend=False,
+                ax=ax,
+                color=colors)
+    
+    # Graficar los datos
+    
+    ax.grid(color='#F5D300', alpha = 0.5)  # Cambiar el grid
+    font = {'fontname' : 'Arial'} # Cambiar el tipo de fuente
+    plt.xlabel("Number of student", **font)
+    plt.ylabel("Score", **font)
+    plt.title("SAT", size = 30, **font)
+    plt.show()
+
+
+def main():
+    data = pd.read_csv('1.01. Simple linear regression.csv') # Leemos la información del CSV
+    Graph(data)
+
+main()
