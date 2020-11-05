cf064df6d62086126ca9bd177c13010d41c0fc8f
Pablo Yamamoto <a01022382@gmail.com>
2020-11-05 17:05:57 -0600

    Notas Importantes

---
+++ importantnotes.py
@@ -0,0 +1,71 @@
+# IMPORTANT for a coefficient to be statistical significant we want a p value less than 0.05.
+# If all betas are 0, then none of the X´s matter -> out model has no merit 
+# The lower the F-statistic, the closer to a non-significant model
+
+
+# Assumptions
+# 1.- Linearity : each independt variable its multiplicated by a coefficient
+# 2.- No endogeneity 
+# 3.- Normality : Error is distributated. expected value is 0
+# 4.- No autocorrelation : Co-variance of any two error terms in zero
+# 5.- No multicolliearity : When two variables have a high correlation between each other
+#
+#
+#
+# LINEARITY 
+# 
+# Each independet variable is multiplied by a coefficeinte and summed up to predict the value of the dependent variable
+# 
+#
+# NO ENDOGENEITY
+#
+# Refers to the prohibition of a link between the independent variables and the error mathematically expressed
+#
+#
+# NORMALITY AND HOMOSCEDASTICITY 
+#
+# Normality : error normally distributated 
+# Homoscedasticity : error term should have equal terms with others
+#
+#
+# NO AUTOCORRELATION
+# 
+# 
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
